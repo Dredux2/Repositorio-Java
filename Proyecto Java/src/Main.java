d11e15b27ae0fdbbd4d707b2b81fef0095157623
DREDUX-LAPTOP\GeoNe <davroddia@alu.edu.gva.es>
2024-10-03 13:02:26 +0200

    Practica 4 + Practica 3

--- Proyecto Java/src/Main.java
+++ Proyecto Java/src/Main.java
@@ -1,26 +1,55 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("¿Como te llamas?");
-        String nombre = sc.next();
+        try {
+            System.out.println("Introduce el multiplicando (3 cifras): ");
+            int Original1 = sc.nextInt();
 
-        int edad = -1;
-        while (edad < 0) {
-            System.out.println("Introduce tu edad:");
-            if (sc.hasNextInt()) { // Verifica si el input es un entero.
-                edad = sc.nextInt();
-                if (edad >= 18) {
-                    System.out.println("Enhorabuena " + nombre + ", ahora puedes votar.");
-                } else if (edad >= 0) {
-                    int menor = 18 - edad;
-                    System.out.println("Lo siento " + nombre + ", no puedes votar.");
-                    System.out.println("Te faltan " + menor + " años para ser mayor de edad.");
-                }
+            System.out.println("Introduce el multiplicador (3 cifras): ");
+            int Original2 = sc.nextInt();
+
+            if ((Original1 > 99 && Original1 < 1000) && (Original2 > 99 && Original2 < 1000)){
+                int result = Original1 * Original2;
+                System.out.println("El resultado es " + result);
+
+                // Numero1
+                String string1 = Integer.toString(Original1);
+                String OriginalString1 = string1.substring(0,3);
+                int OriginalInt1 = Integer.parseInt(OriginalString1);
+
+                // Numero2
+                String string2 = Integer.toString(Original2);
+                String OriginalString2 = string2.substring(0,3);
+
+                //Numeros individuales de Numero 2
+                String IndividualInt1 = string2.substring(0,1);
+                String IndividualInt2 = string2.substring(1,2);
+                String IndividualInt3 = string2.substring(2,3);
+                int Int1 = Integer.parseInt(IndividualInt1);
+                int Int2 = Integer.parseInt(IndividualInt2);
+                int Int3 = Integer.parseInt(IndividualInt3);
+
+                // Multiplicacion
+                int Resultado1 = OriginalInt1 * Int1;
+                int Resultado2 = OriginalInt1 * Int2;
+                int Resultado3 = OriginalInt1 * Int3;
+
+                System.out.println("El proceso es: ");
+                System.out.println("   " + OriginalString1);
+                System.out.println("x  " + OriginalString2);
+                System.out.println("--------");
+                System.out.println("   " + Resultado1);
+                System.out.println(" " + Resultado2 + "x");
+                System.out.println(Resultado3 + "xx");
+                System.out.println("--------");
+                System.out.println(Original1 * Original2);
             } else {
-                System.out.println("Por favor, introduce un número válido.");
-                sc.next(); // Evita el bucle infinito.
+                System.out.println("ERROR: Valores fuera de rango.");
             }
+        } catch (InputMismatchException error) {
+            System.out.println("ERROR: Debes ingresar un numero entero.");
         }
     }
 }
---
+++ Proyecto Java/src/Practica3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Practica3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Como te llamas?");
+        String nombre = sc.next();
+
+        int edad = -1;
+        while (edad < 0) {
+            System.out.println("Introduce tu edad:");
+            if (sc.hasNextInt()) { // Verifica si el input es un entero.
+                edad = sc.nextInt();
+                if (edad >= 18) {
+                    System.out.println("Enhorabuena " + nombre + ", ahora puedes votar.");
+                } else if (edad >= 0) {
+                    int menor = 18 - edad;
+                    System.out.println("Lo siento " + nombre + ", no puedes votar.");
+                    System.out.println("Te faltan " + menor + " años para ser mayor de edad.");
+                }
+            } else {
+                System.out.println("Por favor, introduce un número válido.");
+                sc.next(); // Evita el bucle infinito.
+            }
+        }
+    }
+}
