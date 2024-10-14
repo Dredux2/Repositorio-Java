2c6f2fa120b66666c0331fc1528353fed47201c1
DREDUX-LAPTOP\GeoNe <davroddia@alu.edu.gva.es>
2024-10-14 22:09:13 +0200

    Edicion Ejercicio 4

--- Proyecto Java/src/org/example/Modos.java
+++ Proyecto Java/src/org/example/Modos.java
@@ -1,30 +1,79 @@
 package org.example;
+import java.time.LocalDateTime;
 import java.util.Scanner;
-public class Excepciones {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String num1 = sc.next();
-        int dividendo = 0;
-        int num_entero = 0;
-        int division = 0;
+public class Modos {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        LocalDateTime hoy = LocalDateTime.now();
+        int anyoActual = hoy.getYear();
 
-        if (sc.hasNextInt()){
-            System.out.print("Introduce el dividendo: ");
-            dividendo = sc.nextInt();
+        System.out.println("Programa para calcular el año de nacimiento y la edad");
+        System.out.println("1º Año de nacimiento");
+        System.out.println("2º Edad");
+        System.out.println("Seleciona una opcion: ");
+
+        String opcion = teclado.next();
+        int fechaNacimiento = 1800;
+
+        boolean error = false;
+        if (opcion  .equals("1")) {
+            System.out.println("Introduce un año: ");
+            String anyoNacimiento = teclado.next();
+
+            try {
+                fechaNacimiento = Integer.parseInt(anyoNacimiento);
+            }catch (NumberFormatException e){
+                System.out.println("Has introducido un formato erroneo. no es un numero. ");
+            }
+
+            if (fechaNacimiento < 1900 || fechaNacimiento > anyoActual){
+                System.out.println("El año introducido no es correcto");
+                error = true;
+            }
+
+        } else if (opcion.equals("2")) {
+
+            int edad = 0;
+
+            System.out.println("Introduce una edad: ");
+
+            if (teclado.hasNextInt()){
+
+            }else {
+                System.out.println("La edad introducida no tiene un formato valido");
+            }
+
+            if (edad < 0){
+                System.out.println("La edad introducida no es valida. ");
+                error = true;
+            }else {
+
+                int anyo_nac = anyoActual - edad;
+
+            }
+
         } else {
-            System.out.print("El formato no es numerico");
+            System.out.println("El modo introdicdo no es correcto.");
         }
 
-        try {
-            System.out.print("Introduce el divisior: ");
-            num_entero = Integer.parseInt(num1);
-            division = dividendo  / num_entero;
-        } catch (NumberFormatException e1){
-            System.out.print("No has escrito un numero. " + e1.getMessage());
-        } catch (ArithmeticException e2){
-            System.out.print("El denomidador es 0, division erronea: " + e2.getMessage());
-        } finally {
-            System.out.print("El resultado es: " + division);
+        if (error == false) {
+
+            if (fechaNacimiento >= 1900 && fechaNacimiento <= 1927) {
+                System.out.println("Eres de la generacion sin bautizar.");
+            } else if (fechaNacimiento >= 1928 && fechaNacimiento <= 1944) {
+                System.out.println("Eres de la generacion silet.");
+            } else if (fechaNacimiento >= 1945 && fechaNacimiento <= 1964) {
+                System.out.println("Eres de la generacion baby boomers.");
+            } else if (fechaNacimiento >= 1965 && fechaNacimiento <= 1981) {
+                System.out.println("Eres de la generacion x.");
+            } else if (fechaNacimiento >= 1982 && fechaNacimiento <= 1994) {
+                System.out.println("Eres de la generacion mellenial.");
+            } else if (fechaNacimiento >= 1995 && fechaNacimiento <= anyoActual) {
+                System.out.println("Eres de la generacion centenial.");
+            } else {
+                System.out.println("No eres de ninguna generacion.");
+            }
         }
+
     }
 }
--- Proyecto Java/src/org/example/Practica4.java
+++ Proyecto Java/src/org/example/Practica4.java
@@ -15,39 +15,28 @@ public class Practica4 {
             System.out.println("Introduce el multiplicador (3 cifras): ");
             int Original2 = sc.nextInt();
 
-            // Punto de decision para filtrar los valores permitidos por el programa
             if ((Original1 > 99 && Original1 < 1000) && (Original2 > 99 && Original2 < 1000)){
-                // Multiplicacion para el resultado final y mostrandolo por consola
                 int result = Original1 * Original2;
                 System.out.println("El resultado es " + result);
 
-                // Numero 1:
-                // Transformamos el int proporcionado por el usuario en tipo String
                 String string1 = Integer.toString(Original1);
-                // Identificamos los primeros caracteres introducidos y los almacenamos
                 String OriginalString1 = string1.substring(0,3);
-                // Convertimos esos caracteres identificados en un valor int para una posterior operacion
                 int OriginalInt1 = Integer.parseInt(OriginalString1);
 
-                // Numero 2: Lo mismo que en el anterior pero sin convertirlo al final en int porque no es necesario
                 String string2 = Integer.toString(Original2);
                 String OriginalString2 = string2.substring(0,3);
 
-                // Seleccionamos individualmente cada numero del segundo numero introducido
                 String IndividualInt1 = string2.substring(0,1);
                 String IndividualInt2 = string2.substring(1,2);
                 String IndividualInt3 = string2.substring(2,3);
-                // Los almacenamos en forma de int y asi operar con ellos despues
                 int Int1 = Integer.parseInt(IndividualInt1);
                 int Int2 = Integer.parseInt(IndividualInt2);
                 int Int3 = Integer.parseInt(IndividualInt3);
 
-                // Multiplicamos los 3 numeros del primer numero por cada uno del segundo y guardamos el resultado
                 int Resultado1 = OriginalInt1 * Int1;
                 int Resultado2 = OriginalInt1 * Int2;
                 int Resultado3 = OriginalInt1 * Int3;
 
-                // Interfaz para mostrar el proceso de la multiplicacion
                 System.out.println("El proceso es: ");
                 System.out.println("   " + OriginalString1);
                 System.out.println("x  " + OriginalString2);
@@ -56,13 +45,10 @@ public class Practica4 {
                 System.out.println(" " + Resultado2 + "x");
                 System.out.println(Resultado3 + "xx");
                 System.out.println("--------");
-                // Utilizamos el resultado final previamente calculado
                 System.out.println(result);
             } else {
-                // Excepcion para los casos en los que los valores no correspondan con lo solicitado
                 System.out.println("ERROR: Valores fuera de rango.");
             }
-            // Excepcion manejada por la clase InputMismatchException almacenada en la variable "error"
         } catch (InputMismatchException error) {
             System.out.println("ERROR: Debes ingresar un numero entero.");
         }
